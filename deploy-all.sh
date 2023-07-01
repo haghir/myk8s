e6ea7242c4a55369bf9f8fa93bd10fb4e3c44dc9
haghir <47360153+haghir@users.noreply.github.com>
2023-07-02 02:35:53 +0900

    Modifes the deploy script to ignore the service that is already deployed.

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 ls | while read SNAME ; do
-	if [ -d "$SNAME" ] ; then
-		echo "deploy: ${SNAME}"
-		helm install "${SNAME}" "${SNAME}"
+	if [ ! -d "$SNAME" ] ; then
+		continue
 	fi
+
+	if [ helm status "${SNAME}" ] ; then
+		continue
+	fi
+
+	echo "deploy: ${SNAME}"
+	helm install "${SNAME}" "${SNAME}"
 done
