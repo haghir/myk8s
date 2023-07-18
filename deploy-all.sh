e247bc9ed0af4c727a10f5e967e3c6f37365ddfb
haghir <47360153+haghir@users.noreply.github.com>
2023-07-18 23:47:38 +0900

    Improves the message.

--- deploy-all.sh
+++ deploy-all.sh
@@ -5,10 +5,12 @@ ls | while read SNAME ; do
 		continue
 	fi
 
+	echo ""
+	echo "deploying ${SNAME}"
+
 	if helm status "${SNAME}" ; then
 		continue
 	fi
 
-	echo "deploy: ${SNAME}"
 	helm install "${SNAME}" "${SNAME}"
 done
